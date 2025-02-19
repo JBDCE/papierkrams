cd3c3509c327e8f59102e3077fded6a30b2e921d
JBDCE <61785226+JBDCE@users.noreply.github.com>
2025-02-19 21:41:27 +0100

    Add initial attempt on parsing content of file
    This uses a samplefile which is not included in git

--- config.py
+++ config.py
@@ -1,2 +1,28 @@
 paperless_url = '192.168.176.28'
 paperless_port = '8000'
+
+interesting_fields = [
+    # The Issuer is assumed to be contained in a single row.
+    # The first result of the file should yield the target most of the time
+    {
+        'fieldname': 'issuer',
+        'api_field': None,
+        'regex_string': r'^.*\d{5,}.*$',
+        'regex_index': 0,
+    },
+    # Use the field paperless provides for this
+    {
+        'fieldname': 'issuedate',
+        'api_field': 'created_date',
+        'regex_string': None,
+        'regex_index': None,
+    },
+    # Good luck
+    # Maybe the last index that contains words like "summe" or "gesamt"
+    {
+        'fieldname': 'payment_total',
+        'api_field': None,
+        'regex_string': None,
+        'regex_index': None,
+    }
+]
