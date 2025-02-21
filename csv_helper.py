33c9988e5b0f9d4bd90a0fb300f59dc5e11cd3a6
JBDCE <61785226+JBDCE@users.noreply.github.com>
2025-02-21 19:32:23 +0100

    Finish first iteration of csv helper
    Also adds temp files to gitignore to get rid of them

--- csv_helper.py
+++ csv_helper.py
@@ -1,6 +1,13 @@
-from csv import writer
+from csv import writer, QUOTE_ALL
 
 def write_file(filepath, data):
+    with open(filepath, 'w') as outfile:
+        wr = writer(outfile, quoting=QUOTE_ALL, lineterminator=';\n')
+
+        # Add a header with the column names just for convenience
+        wr.writerow(data[0].keys())
+        for row in data:
+            wr.writerow(row.values())
     return
 
 if __name__ == '__main__':
@@ -21,6 +28,8 @@ if __name__ == '__main__':
             'sum': 34.5,
         }
     ]
-    
-    write_file('outfile.csv', sample_data)
 
+    write_file(
+        filepath='outfile.csv',
+        data=sample_data
+    )
