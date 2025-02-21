a8c27681a1ea9dcc3b831685fd38056ee7d534d2
JBDCE <61785226+JBDCE@users.noreply.github.com>
2025-02-21 20:59:58 +0100

    Update config with outfile columns

--- config.py
+++ config.py
@@ -1,5 +1,30 @@
-paperless_url = '192.168.176.28'
+paperless_url = '192.168.176.71'
 paperless_port = '8000'
+
+
+outfile_columns = [
+    {
+        'name': 'Zahlungsempfänger',
+        'field_name': 'correspondent',
+    },
+    {
+        'name': 'Ausstellungsdatum',
+        'field_name': 'created_date',
+    },
+    {
+        'name': 'Gesammtsumme',
+        'field_name': 'custom_fields',
+        'custom_field_id': 3,
+    },
+    {
+        'name': 'Anliegerwohnungsanteil in %',
+        'field_name': 'custom_fields',
+        'custom_field_id': 2,
+    }
+]
+
+
+
 
 interesting_fields = [
     # The Issuer is assumed to be contained in a single row.
